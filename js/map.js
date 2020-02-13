86aa34a0a60c0b086ea81a1fa24a883a23117880
Fernando Vieira <nandovieira2@yahoo.com.br>
2020-02-13 02:44:49 -0300

    commit inicial

---
+++ js/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGlyYXp1a3kiLCJhIjoiY2p2eW1sejl5MGhvcDRhcDh2NXB5NDV4aiJ9.ax9uzCdL5uFOio7E0r3tVA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [-43.938286, -19.918855],
+    zoom: 14
+});
